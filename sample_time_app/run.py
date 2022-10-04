5db56c2a812020b373f7ad1f81c79b84a093a834
rumidesai <104800299+rumidesai@users.noreply.github.com>
2022-10-03 23:16:57 -0400

    DCN_LAB2

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/time')
+def display_time():
+    return str(datetime.datetime.now()) + ' is the current time.'
 
 
 app.run(host='0.0.0.0',
